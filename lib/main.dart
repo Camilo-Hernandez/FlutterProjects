17a971101596699592ef969e0de2e41ea47241d4
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2023-03-06 13:42:20 -0500

    Añadida página inicial
    Incluye:
    MaterialApp() y CuppertinoApp() con sus ligeras diferencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'AddBookButton.dart';
@@ -6,7 +7,15 @@ import 'BookDescription.dart';
 void main() {
   // MaterialApp() maneja muchísimas características que los developers no tenenmos que gestionar manualmente.
   // Sigue los lineamientos de Material Design de Google publicados en m3.material.io
+  /*
   runApp(const MaterialApp(
+    home: Booksy(),
+    title: "Booksy",
+  ));
+  */
+
+  // Ahora con Cupertino:
+  runApp(const CupertinoApp(
     home: Booksy(),
     title: "Booksy",
   ));
@@ -19,38 +28,60 @@ class Booksy extends StatelessWidget {
   Widget build(BuildContext context) {
     var bookCoverURI = "res/cover.jpg";
 
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      color: Colors.white,
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(top: 20),
-          ),
-          Container(
-            decoration: const BoxDecoration(
-              boxShadow: [BoxShadow(blurRadius: 8)],
-              color: Colors.white,
-              backgroundBlendMode: BlendMode.lighten,
+/*    return Scaffold( // Con MaterialApp
+      appBar: AppBar(
+        title: const Text("Booksy"),
+      ),
+        body: Container(
+*/
+    return CupertinoPageScaffold(
+      // Con Cupertino
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text("Booksy"),
+      ),
+      child: Container(
+        // Hasta aquí Cupertino
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        color: Colors.white,
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      boxShadow: [BoxShadow(blurRadius: 8)],
+                      color: Colors.white,
+                      backgroundBlendMode: BlendMode.lighten,
+                    ),
+                    child: Image(
+                      image: AssetImage(bookCoverURI),
+                      width: 200,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 40),
+                  ),
+                  const BookDescription(
+                    "The Hitchhiker's Guide to the Galaxy",
+                    "Douglas Adams",
+                    "It's an ordinary Thursday morning for Arthur Dent... until his house gets demolished. The Earth follows shortly after to make way for a new hyperspace express route, and Arthur's best friend has just announced that he's an alien.",
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10),
+                  ),
+                  const AddBookButton(),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 5),
+                  )
+                ],
+              ),
             ),
-            child: Image(
-              image: AssetImage(bookCoverURI),
-              width: 200,
-            ),
           ),
-          const Padding(
-            padding: EdgeInsets.only(top: 40),
-          ),
-          const BookDescription(
-            "The Hitchhiker's Guide to the Galaxy",
-            "Douglas Adams",
-            "It's an ordinary Thursday morning for Arthur Dent... until his house gets demolished. The Earth follows shortly after to make way for a new hyperspace express route, and Arthur's best friend has just announced that he's an alien.",
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 5),
-          ),
-          const AddBookButton(),
-        ],
+        ),
       ),
     );
   }
