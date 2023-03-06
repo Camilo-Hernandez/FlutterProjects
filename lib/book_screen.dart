5285f2cacff4bac7dde5de190ae28bb2f4a410c7
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2023-03-06 15:13:11 -0500

    Añadido book_screen.dart y refactorizado los nombres de las clases a snake_case

---
+++ lib/book_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'add_book_button.dart';
+import 'book_description.dart';
+
+class BookScreen extends StatelessWidget {
+  final String bookTitle;
+  final String authorName;
+  final String bookDescription;
+  final String bookCoverURI;
+
+  const BookScreen(
+      {super.key,
+      required this.bookTitle,
+      required this.authorName,
+      required this.bookDescription,
+      required this.bookCoverURI});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      color: Colors.white,
+      child: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 20),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    boxShadow: [BoxShadow(blurRadius: 8)],
+                    color: Colors.white,
+                    backgroundBlendMode: BlendMode.lighten,
+                  ),
+                  child: Image(
+                    image: AssetImage(bookCoverURI),
+                    width: 200,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 40),
+                ),
+                BookDescription(
+                  bookTitle,
+                  authorName,
+                  bookDescription,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                ),
+                const AddBookButton(),
+                const Padding(
+                  padding: EdgeInsets.only(top: 5),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:booksy/UserData.dart';
+import 'package:booksy/user_data.dart';
 import 'package:flutter/material.dart';
 
-import 'AddBookButton.dart';
-import 'BookDescription.dart';
+import 'book_screen.dart';
 
 void main() {
   // MaterialApp() maneja muchísimas características que los developers no tenenmos que gestionar manualmente.
@@ -23,12 +22,15 @@ void main() {
 class Booksy extends StatelessWidget {
   const Booksy({super.key});
 
-  static const booksIds = ["douglas-hitch"];
+  static const bookTitle = "The Hitchhiker's Guide to the Galaxy";
+  static const authorName = "Douglas Adams";
+  static const bookDescription =
+      "It's an ordinary Thursday morning for Arthur Dent... until his house gets demolished. The Earth follows shortly after to make way for a new hyperspace express route, and Arthur's best friend has just announced that he's an alien.";
+  static const bookCoverURI = "res/cover.jpg";
+  static const List<String> booksIds = ["douglas-hitch"];
 
   @override
   Widget build(BuildContext context) {
-    var bookCoverURI = "res/cover.jpg";
-
 /*
     return CupertinoPageScaffold(
       // Con Cupertino
@@ -43,51 +45,14 @@ class Booksy extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Booksy"),
       ),
-      body: UserData(
-          booksIds:
-              booksIds, // se obtiene de una base de datos, consultando al backend
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            color: Colors.white,
-            child: SafeArea(
-              child: Center(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(top: 20),
-                      ),
-                      Container(
-                        decoration: const BoxDecoration(
-                          boxShadow: [BoxShadow(blurRadius: 8)],
-                          color: Colors.white,
-                          backgroundBlendMode: BlendMode.lighten,
-                        ),
-                        child: Image(
-                          image: AssetImage(bookCoverURI),
-                          width: 200,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 40),
-                      ),
-                      const BookDescription(
-                        "The Hitchhiker's Guide to the Galaxy",
-                        "Douglas Adams",
-                        "It's an ordinary Thursday morning for Arthur Dent... until his house gets demolished. The Earth follows shortly after to make way for a new hyperspace express route, and Arthur's best friend has just announced that he's an alien.",
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 10),
-                      ),
-                      const AddBookButton(),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 5),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
+      body: const UserData(
+          booksIds: booksIds,
+          // se obtiene de una base de datos, en servidor por ejemplo, consultando al backend
+          child: BookScreen(
+            bookTitle: bookTitle,
+            authorName: authorName,
+            bookDescription: bookDescription,
+            bookCoverURI: bookCoverURI,
           )),
     );
   }
