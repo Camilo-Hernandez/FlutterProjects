300eb3aa2348a3ce78b201388b9f0274fc48b696
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2023-03-09 16:30:18 -0500

    "Completado" el ejemplo de TDD de Uncle Bob

--- lib/testing_examples/my_stack.dart
+++ lib/testing_examples/my_stack.dart
@@ -1,18 +1,27 @@
 // First in, last out
 
 class Stack {
-  bool _isEmpty = true;
+  int size = 0;
+  late List<int> elements = [];
 
   isEmpty() {
-    return _isEmpty;
+    return size == 0;
   }
 
-  void push(int i) {
-    _isEmpty = false;
+  void push(int _element) {
+    this.elements.insert(size++, _element);
   }
 
   int pop() {
-    throwUnderFlowError();
+    if (isEmpty()) {
+      throwUnderFlowError();
+    } else {
+      // This is the wrong solution, it is not scalable.
+      // isEmpty = true;
+      // Then let's create a test that prove it! -> Push 2 Pop 1.
+      // It's a forceful test -> you know it is the wrong solution!
+      return elements.removeAt(--size);
+    }
   }
 
   // Never tiene 3 utilidades. Entre ellas, es sólo lanzar un error.
--- test/testing_examples/my_stack_test.dart
+++ test/testing_examples/my_stack_test.dart
@@ -2,7 +2,11 @@
 1. You are not allowed to write any production code until your have first written a test that fails because the production code doesn't exist.
 2. You are not allowed to write any more of a unit test than is sufficient to fail - and not compiling is failing.
 3. You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
-4. Engage as few brain cells as possible because you will need them later -> Keep it simple! Don't do too much too fast.
+4. Engage as few brain cells as possible, you will need them later. Keep it simple! (KISS) Don't do too much too fast.
+5. Try to avoid the core (the golden behaviour) of the problem until it is unavoidable. Save the most important feature of as long as possible.
+6. If you think some code you just wrote is a wrong solution... prove it! Put it to a test.
+7. You are not allowed to make the production code more specific than the tests. The tests and the production code must move in opposite directions.
+    Every new test you add makes the tests more specific, more constrained. Everything you do to the production code must make it more general.
 
 Following these 3 rules, you would have a documentation that'll be the envy of the world.
 It will be even more fun!
@@ -38,7 +42,7 @@ void main() {
       'Stack',
       () => {
             test('.isEmpty() is true after creating the stack', () {
-              expect(stack.isEmpty(), true);
+              expect(stack.isEmpty(), equals(true));
             }),
             test('.isEmpty() is false after pushing 0', () {
               stack.push(0);
@@ -50,6 +54,29 @@ void main() {
             test('.pop() throws UnderFlow in an empty stack', () {
               // Dado que throwUnderFlowError es tipo Never, se pone empaquetado
               expect(() => stack.pop(), throwsA(isRangeError));
+            }),
+            test('after push 0 and a pop, stack will be empty', () {
+              stack.push(0);
+              stack.pop(); // first this will fail
+              expect(stack.isEmpty(), equals(true));
+            }),
+            test('after two pushes and one pop, stack will not be empty', () {
+              stack.push(0);
+              stack.push(0);
+              stack.pop();
+              expect(stack.isEmpty(), equals(false));
+            }),
+            test('after pushing X will pop X, stack will be empty', () {
+              stack.push(99);
+              expect(stack.pop(), 99);
+              stack.push(88);
+              expect(stack.pop(), 88);
+            }),
+            test('after pushing X and Y will pop Y and X', () {
+              stack.push(99);
+              stack.push(88);
+              expect(stack.pop(), 88);
+              expect(stack.pop(), 99);
             }),
           });
 }
